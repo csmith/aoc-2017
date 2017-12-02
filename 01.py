1f3dd13e0ccc70cb0cb12352ae688e88658af499
Chris Smith <chris@chameth.com>
2017-12-02 20:31:29 +0000

    f-strings
    And Python3 fixes....

--- 01.py
+++ 01.py
@@ -1,12 +1,12 @@
 with open('data/01.txt', 'r') as file:
     captcha = file.readline().strip() * 2
     length = len(captcha) // 2
-    print('Part one: %s' % sum(int(captcha[i]) for i in range(length) if captcha[i] == captcha[i+1]))
-    print('Part two: %s' % sum(int(captcha[i]) for i in range(length) if captcha[i] == captcha[i + length // 2]))
+    print(f'Part one: {sum(int(captcha[i]) for i in range(length) if captcha[i] == captcha[i+1])}')
+    print(f'Part two: {sum(int(captcha[i]) for i in range(length) if captcha[i] == captcha[i + length // 2])}')
 
 # Alternatively...
 import numpy as np
 with open('data/01.txt', 'r') as file:
     captcha = list(file.readline().strip())
 for part, roll in enumerate([1, len(captcha)//2]):
-    print('Part %s: %s' % (part + 1, sum(int(i) for i, j in zip(captcha, np.roll(captcha, roll)) if i == j)))
+    print(f'Part {part+1}: {sum(int(i) for i, j in zip(captcha, np.roll(captcha, roll)) if i == j)}')
--- 02.py
+++ 02.py
@@ -1,5 +1,5 @@
 import itertools
 with open('data/02.txt', 'r') as file:
-    sheet = map(lambda row: map(int, row.split('\t')), file.readlines())
-    print('Part one: %s' % sum(max(row) - min(row) for row in sheet))
-    print('Part two: %s' % sum(next(a//b for a,b in itertools.permutations(row, 2) if a % b == 0) for row in sheet))
+    sheet = list(map(lambda row: list(map(int, row.split('\t'))), file.readlines()))
+    print(f'Part one: {sum(max(row) - min(row) for row in sheet)}')
+    print(f'Part two: {sum(next(a//b for a,b in itertools.permutations(row, 2) if a % b == 0) for row in sheet)}')
