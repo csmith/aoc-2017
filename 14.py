f01f792a1991087f51c017a602635ee3fe3b2fed
Chris Smith <chris.smith@bridgeinternationalacademies.com>
2017-12-14 12:35:39 +0000

    Further optimise day 14.
    Flatten the grid into a single string (separated by line-feeds to
    side-step wrap-around problems) and use the offset in the string
    to identify cells instead of (x, y) tuples.
    
    Don't calculate part one separately, just do it as we go.

--- 14.py
+++ 14.py
@@ -2,12 +2,13 @@ from shared import knot_hash, add_connected_components
 
 with open('data/14.txt', 'r') as file:
     seed = file.readline().strip()
-    grid = [bin(int(knot_hash(f'{seed}-{i}'), 16))[2:].zfill(128) for i in range(128)]
-    print(f'Part one: {sum(row.count("1") for row in grid)}')
+    grid = '\n'.join([bin(int(knot_hash(f'{seed}-{i}'), 16))[2:].zfill(128) for i in range(128)])
 
+    count = 0
     regions = []
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == '1':
-                add_connected_components(regions, [(x-1, y), (x, y-1)], {(x, y)})
+    for offset, cell in enumerate(grid):
+        if cell == '1':
+            count += 1
+            add_connected_components(regions, [offset-1, offset-129], {offset})
+    print(f'Part one: {count}')
     print(f'Part two: {len(regions)}')
